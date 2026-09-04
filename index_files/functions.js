fd6f873a361e8fe632c4742221cbfb2e4bc0bc48
ntsakombungela2011-a11y <ntsakombungela2011@gmail.com>
2026-09-04 21:31:17 +0200

    Update functions.js

--- index_files/functions.js
+++ index_files/functions.js
@@ -7,14 +7,6 @@ var $win = $(window);
 var clientWidth = $win.width();
 var clientHeight = $win.height();
 
-$(window).resize(function() {
-    var newWidth = $win.width();
-    var newHeight = $win.height();
-    if (newWidth != clientWidth && newHeight != clientHeight) {
-        location.replace(location);
-    }
-});
-
 (function($) {
 	$.fn.typewriter = function(onComplete) {
 		this.each(function() {
