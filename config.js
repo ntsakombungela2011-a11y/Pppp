5d5c5acfafb764f7fbbc2de368469f0ce2e7e65b
ntsakombungela2011-a11y <ntsakombungela2011@gmail.com>
2026-09-04 15:58:10 +0200

    Add files via upload

---
+++ config.js
@@ -0,0 +1,16 @@
+var config = {
+    lines: [
+        "Boipelo,",
+        "In chess game every piece has a purpose ,",
+        "but from the first day you weren't just a piece on my board,",
+        "you became the reason I wanted to play at all, the reason I wanted to live.",
+        "You're my queen, moving through my life with a power, grace and elegance nothing else compares to,",
+        "and every plan I make always leads back to you.",
+        "I love you with all my heart.",
+        "So here's my move Qxd8+, no hidden strategy behind it...",
+    ],
+    // Format: YYYY-MM-DDTHH:MM:SS
+    date: "2026-07-21T00:00:00",
+    // The two names
+    names: ["Boipelo", "Ntsako"]
+}
